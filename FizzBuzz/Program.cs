c435e74d6f5da13f1514158d07fcd6a3f377d283
schreiner <thomas.schreiner@fleischpruefring.de>
2017-03-14 12:13:31 +0100

    FizzBuzz erste Version

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int zahl = 0; zahl < 100; zahl++)
+            {
+                Console.Write(zahl);
+                Console.Write(" ");
+                int fizz = zahl % 3;
+                int buzz = zahl % 5;
+       
+                if (fizz == 0) { Console.Write("Fizz");}
+                if (buzz == 0) { Console.Write("Buzz"); }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
